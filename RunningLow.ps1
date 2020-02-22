8d4ef78dbe5b0c30bdd31e7843d096a8b30cb346
Ole Marius Løset <olemariusloset@gmail.com>
2020-02-22 20:33:53 +0100

    customized for bloc

--- RunningLow.ps1
+++ RunningLow.ps1
@@ -13,45 +13,28 @@
 #
 ###
 
-
 # Command-line parameters
 param(
-  # - minSize : the minimum free disk space acceptable threshold: any checked drive with less available space will raise a warning.
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
   [string] $minSize = 20GB,
-
-  # - hosts: if specified, will also check the disk space on the given colon-separated list of hostnames (machine names OR ip addresses) within the LAN.
-  #            Example: $hosts = "HOSTNAME1:HOSTNAME2:129.168.0.115";
-  #           IMPORTANT: Connecting to remote machines will require launching RunningLow with elevated priviledges
-  #           and the Windows Management service up, running and reachable (TCP port 5985) on the remote machine.
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
   [string] $hosts = $null,
-
-  # - volumes: a colon-separated list of the drive volumes (letters) to check: set it to $null to check all local (non-network) drives.
-  #            Example: $volumes = "C:D";
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
   $volumes = $null,
-
-  # - email_to : if specified, will send a low-disk-space warning email to the given colon-separated addresses.
-  #              Example: $email_to = "my@email.com:your@email.com";
-  #              Default is $null (no e-mail will be sent). Replace it with your@email.com if you don't want to set it from the CLI.
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  [string] $email_to = $null,
-
-  # These parameters can be used to set your SMTP configuration: username, password & so on. 
-  # It's strongly advisable to set them within the code instead of setting them from the CLI, as you might rarely want to change them.
+  [string] $email_to ="olemariusloset@gmail.com",
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_username = "username@yourdomain.com",
+  $email_username = "bloc-alerts@outlook.com",
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_password = "yourpassword",
+  $email_password = "BlocMafiaBlocForAlertEmailAccount",
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_smtp_host = "smtp.yourdomain.com",
+  $email_smtp_host = "smtp.outlook.com",
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_smtp_port = 25,
+  $email_smtp_port = 587,
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_smtp_SSL = 0,
+  $email_smtp_SSL = $true,
   [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$false)]
-  $email_from = "username@yourdomain.com"
+  $email_from = $email_username
 )
 
 $sep = ":";
@@ -73,14 +56,14 @@ foreach ($cur_host in $hosts.split($sep))
 	Write-Host ("----------------------------------------------");
     $drives_to_check = @();
 
-    if ($volumes -eq $null) {
+    if ($null -eq $volumes) {
    	    $volArr = 
           If ($cur_host -eq $env:computername) { Get-WMIObject win32_volume }
           Else { Invoke-Command -ComputerName $cur_host -ScriptBlock { Get-WMIObject win32_volume } }
 
 	    $drives_to_check = @();
         foreach ($vol in $volArr | Sort-Object -Property DriveLetter) {
-	        if ($vol.DriveType -eq 3 -And $vol.DriveLetter -ne $null ) {
+	        if ($vol.DriveType -eq 3 -And $null -ne $vol.DriveLetter ) {
   		        $drives_to_check += $vol.DriveLetter[0];
 		    }
 	    }
@@ -95,26 +78,21 @@ foreach ($cur_host in $hosts.split($sep))
 
 	if ($disk.Free -lt $minSize) {
 		Write-Host "  - [" -noNewLine
-        Write-Host "KO" -noNewLine -ForegroundColor Red
+        Write-Host "WARNING" -noNewLine -ForegroundColor Red
         Write-Host "] " -noNewLine
-        Write-Host ("Drive " + $d + " has less than " + $minSize `
-			+ " bytes free (" + $disk.Free + " bytes - " + ($disk.Free/1MB).ToString(".00") + " GB)") -noNewLine
+        Write-Host ("Drive " + $d + " has less than " + ($minSize/1GB).ToString(".00") + "GB (" + ($disk.Free/1GB).ToString(".00") + "GB)") -NoNewline
          
         if ($email_to) {
-            Write-Host(": sending e-mail..."); -noNewLine
-	
+            Write-Host(": Sending e-mail...");
 		    $message = new-object Net.Mail.MailMessage;
 		    $message.From = $email_from;
 		    foreach ($to in $email_to.split($sep)) {
 			    $message.To.Add($to);
 		    }
-		    $message.Subject = 	("[RunningLow] WARNING: " + $cur_host + " drive " + $d);
-		    $message.Subject +=	(" has less than " + $minSize + " bytes free ");
-		    $message.Subject +=	("(" + $disk.Free + " bytes - "+ ($disk.Free/1GB).ToString(".00") +" GB)");
-		    $message.Body =		"Hello there, `r`n`r`n";
-		    $message.Body += 	"this is an automatic e-mail message ";
-		    $message.Body += 	"sent by RunningLow Powershell script ";
-		    $message.Body += 	("to inform you that " + $env:computername + " drive " + $d + " ");
+		    $message.Subject = 	("WARNING: " + $cur_host + " drive " + $d);
+		    $message.Subject +=	(" has less than " + ($minSize/1GB).ToString(".00") + "GB free ");
+		    $message.Subject +=	("(" + ($disk.Free/1GB).ToString(".00") +" GB)");
+		    $message.Body += 	("The " + $env:computername + " drive " + $d + " ");
 		    $message.Body += 	"is running low on free space. `r`n`r`n";
 		    $message.Body += 	"--------------------------------------------------------------";
 		    $message.Body +=	"`r`n";
@@ -128,23 +106,21 @@ foreach ($cur_host in $hosts.split($sep))
 		        $message.Body += ($ip.IPAddress + " ");
 		    }
 		    $message.Body += 	"`r`n";
-		    $message.Body += 	("Used space on drive " + $d + ": " + $disk.Used + " bytes. `r`n");
-		    $message.Body += 	("Free space on drive " + $d + ": " + $disk.Free + " bytes. `r`n");
+		    $message.Body += 	("Used space on drive " + $d + ": " + ($disk.Used/1GB).ToString(".00") + " GB. `r`n");
+		    $message.Body += 	("Free space on drive " + $d + ": " + ($disk.Free/1GB).ToString(".00") + " GB. `r`n");
 		    $message.Body += 	"--------------------------------------------------------------";
 		    $message.Body +=	"`r`n`r`n";
 		    $message.Body += 	"This warning will fire when the free space is lower ";
-		    $message.Body +=	("than " + $minSize + " bytes `r`n`r`n");
-		    $message.Body += 	"Sincerely, `r`n`r`n";
-		    $message.Body += 	"-- `r`n";
-		    $message.Body += 	"RunningLow`r`n";
-                    $message.Body +=        "http://www.ryadel.com/RunningLow";
+		    $message.Body +=	("than " + ($minSize/1GB).ToString(".00") + "GB `r`n`r`n");
+
 
 		    $smtp = new-object Net.Mail.SmtpClient($email_smtp_host, $email_smtp_port);
 		    $smtp.EnableSSL = $email_smtp_SSL;
 		    $smtp.Credentials = New-Object System.Net.NetworkCredential($email_username, $email_password);
 		    $smtp.send($message);
-		    $message.Dispose();
-		    Write-Host " E-Mail sent!" ; 
+			$message.Dispose();
+			Write-Host $message.Body;
+		    Write-Host "E-Mail sent!" ; 
         }
         Else {
             Write-Host(".");
@@ -154,7 +130,7 @@ foreach ($cur_host in $hosts.split($sep))
 		Write-Host "  - [" -noNewLine
         Write-Host "OK" -noNewLine -ForegroundColor Green
         Write-Host "] " -noNewLine
-		Write-Host ("Drive " + $d + " has more than " + $minSize + " bytes free: nothing to do.")
+		Write-Host ("Drive " + $d + " has more than " + ($minSize/1GB).ToString(".00") + " GB free: nothing to do.")
 	}
   }
 }
